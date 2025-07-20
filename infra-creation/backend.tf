fcccf83ecca475fbda2a40d8c5231a80dc0117a1
Aishuvthija <168397436+Aishuvthija@users.noreply.github.com>
2025-07-20 20:09:29 +0530

    Update backend.tf

--- infra-creation/backend.tf
+++ infra-creation/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "k8s-infra-creation"
+    bucket = "k8s-infra-creation-aishu-001"
     key    = "tfstate/k8sprod.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-state"
